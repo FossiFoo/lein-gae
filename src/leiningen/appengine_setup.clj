69cf4730d281e8726b2bf837eaab564dc3b4d7a9
Moritz Ulrich <ulrich.moritz@googlemail.com>
2010-01-10 03:25:00 +0100

    appengine-setup now creates web.xml and appengine-web.xml in war/WEB-INF/

--- src/leiningen/appengine_setup.clj
+++ src/leiningen/appengine_setup.clj
@@ -1,5 +1,7 @@
 (ns leiningen.appengine-setup
-  (:use clojure.contrib.pprint)
+  (:use clojure.contrib.pprint
+        [clojure.contrib.duck-streams :only [slurp* spit]])
+  (:require [clojure.contrib.str-utils2 :as str])
   (:import (java.io File FileReader PushbackReader
 		    FileWriter)))
 
@@ -10,34 +12,57 @@
 	     '[com.google.appengine/appengine-tools-sdk "1.2.1"]
 	     '[geronimo-spec/geronimo-spec-servlet "2.4-rc4"]))
 
+(defn- copy-web-xml [project-name]
+  "Creates a standard web.xml in war/WEB-INF/"
+  (spit "war/WEB-INF/web.xml"
+        (reduce (fn [str [pat ret]] (str/replace str pat ret))
+                (slurp* (.getResourceAsStream (clojure.lang.RT/baseLoader)
+                                              "web.xml"))
+                {"$DISPLAY-NAME" project-name
+                 "$SN"  project-name
+                 "$SERVLET-CLASS" (str project-name ".servlet")})))
+
+(defn- copy-appengine-web-xml [appid appversion]
+  "Creates a standard appengine-web.xml in war/WEB-INF/"
+  (spit "war/WEB-INF/appengine-web.xml"
+        (reduce (fn [str [pat ret]] (str/replace str pat ret))
+                (slurp* (.getResourceAsStream (clojure.lang.RT/baseLoader)
+                                              "appengine-web.xml"))
+                {"$APPID" appid
+                 "$APPVERSION" (str appversion)})))
+
 (defn appengine-setup [project]
   (doseq [f ["war/WEB-INF/classes"
-	     "war/WEB-INF/lib"
-	     (format "src/%s" (:name project))]]
+             "war/WEB-INF/lib"
+             (format "src/%s" (:name project))]]
     (.mkdirs (File. (:root project) f)))
+
+  (copy-web-xml (:name project) (:name project))
+  (copy-appengine-web-xml (:name project) 1)
+  
   (binding [*out* (-> project :root (File. (format "src/%s/servlet.clj" (:name project)))
-		      FileWriter.)]
+                      FileWriter.)]
     (prn (list 'ns (symbol (format "%s.servlet" (:name project)))
-	       '(:gen-class :extends javax.servlet.http.HttpServlet)
-	       '(:use compojure.html compojure.http)))
+               '(:gen-class :extends javax.servlet.http.HttpServlet)
+               '(:use compojure.html compojure.http)))
     (prn (list 'defroutes (symbol (:name project))
-	       '(GET "/*" (html [:h1 "Hello World"]))))
+               '(GET "/*" (html [:h1 "Hello World"]))))
     (prn (list 'defservice (symbol (:name project)))))
   (let [[_ n v & proj] (with-open [f (-> project :root (File. "project.clj")
-				  FileReader. PushbackReader.)]
-		  (read f))]
+                                         FileReader. PushbackReader.)]
+                         (read f))]
     (with-open [f (-> project :root (File. "project.clj")
-		      FileWriter.)]
+                      FileWriter.)]
       (binding [*out* f]
-	(pprint 
-	 (seq
-	  (reduce into ['defproject n v]
-		  (update-dependencies
-		   (merge-with (comp first list)  (apply hash-map proj)
-			       {:compile-path "war/WEB-INF/classes"
-                    :library-path "war/WEB-INF/lib"
-                    :resource-path "war/"
-                    :appengine {:id (:name project)
-                                :display-name (:name project)
-                                :version 1
-                                :devport 8080}})))))))))
+        (pprint 
+         (seq
+          (reduce into ['defproject n v]
+                  (update-dependencies
+                   (merge-with (comp first list)  (apply hash-map proj)
+                               {:compile-path "war/WEB-INF/classes"
+                                :library-path "war/WEB-INF/lib"
+                                :resource-path "war/"
+                                :appengine {:id (:name project)
+                                            :display-name (:name project)
+                                            :version 1
+                                            :devport 8080}})))))))))
